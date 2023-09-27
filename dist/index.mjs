124aa7ab23a71062c0820680d5e892621f8fc75b
Evan Brass <brassevan@gmail.com>
2023-09-27 08:18:59 -0700

    remove blob

--- dist/index.mjs
+++ dist/index.mjs
@@ -6,7 +6,6 @@ export * as sqlite_def from './sqlite_def.mjs';
 export * from './asyncify.mjs';
 export * from './func.mjs';
 export * from './conn.mjs';
-export * from './blob.mjs';
 export * from './pool.mjs';
 export * from './value.mjs';
 export {sqlite3, vfs};
