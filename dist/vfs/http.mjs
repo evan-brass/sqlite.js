50ac8ffb8211a06b94630b9a1079495f3531c642
Evan Brass <brassevan@gmail.com>
2023-10-05 17:46:35 -0700

    http vfs rewrite
    Reduce async and handle servers that don't support ranged queries by downloading the db as a blob and then performing queries on the blob.

--- dist/vfs/http.mjs
+++ dist/vfs/http.mjs
@@ -6,80 +6,89 @@ import {
 
 // Example DB: https://phiresky.github.io/world-development-indicators-sqlite/split-db/db.sqlite3.000
 
-export class HttpFile {
+function check_err(resp) {
+	if (resp.ok) return;
+	throw new Error(`HTTP VFS:(${resp.status}) ${resp.statusText}`)
+}
+
+class BlobFile {
+	#inner;
 	flags;
-	url;
-	headers;
 	sector_size = 0;
-	#size;
-	constructor(url, size, headers = {}, flags) {
-		this.url = url;
-		this.#size = size;
-		this.headers = headers;
+	constructor(blob, flags) {
+		this.#inner = blob;
 		this.flags = flags;
 	}
 	// IO:
 	device_characteristics() { return SQLITE_IOCAP_IMMUTABLE; }
 	async read(offset, len) {
-		if (!this.#size) return new Uint8Array();
-		const headers = Object.create(this.headers);
+		offset = Number(offset);
+		return new Uint8Array(await this.#inner.slice(offset, offset + len).arrayBuffer());
+	}
+	size() { return this.#inner.size; }
+	close() {}
+	file_control(_op, _arg) { return SQLITE_NOTFOUND; }
+}
+class HttpFile {
+	#url;
+	#size = false;
+	flags;
+	constructor(response, flags) {
+		this.flags = flags;
+		this.#url = response.url;
+	}
+	sector_size = 0;
+	// IO:
+	device_characteristics() { return SQLITE_IOCAP_IMMUTABLE; }
+	async read(offset, len) {
 		// Gosh, I really wish Math.min worked with BigInts.
 		let end = offset + BigInt(len);
-		if (end > this.#size) end = this.#size;
 		end -= 1n; // Byte ranges are inclusive for some reason...
-		headers['Range'] = `bytes=${offset}-${end}`;
-		const resp = await fetch(this.url, { headers });
-		if (!resp.ok) throw new Error(`HTTP VFS Error (${resp.status}): ${resp.statusText}`);
+		const resp = await fetch(this.#url, { headers: {
+			'Range': `bytes=${offset}-${end}`
+		} });
+		check_err(resp);
+		if (resp.status !== 206) throw new Error("Received a non-partial response.");
 
 		return new Uint8Array(await resp.arrayBuffer());
 	}
 	size() {
-		return this.#size;
+		if (this.#size !== false) return this.#size;
+		return (async () => {
+			const resp = await fetch(this.#url, {method: 'head'});
+			check_err(resp);
+			this.#size = BigInt(resp.headers.get('Content-Length'));
+			return this.#size;
+		})();
 	}
 	close() {}
-	async file_control(_op, _arg) { return SQLITE_NOTFOUND; }
+	file_control(_op, _arg) { return SQLITE_NOTFOUND; }
 }
 
 export class Http {
 	name = 'http';
-	max_pathname = 128;
-	flags_filter = SQLITE_OPEN_READONLY;
+	max_pathname = 255;
 	async open(filename, flags) {
-		const secure = filename.get_parameter('https', location?.protocol == 'https');
-		filename = String(filename);
-		let url;
-		if (filename.startsWith('http')) {
-			url = new URL(filename);
-		} else if (filename.startsWith('//')) {
-			url = new URL(`${secure ? 'https' : 'http'}:${filename}`);
+		flags &= SQLITE_OPEN_READONLY;
+		const url = new URL(String(filename));
+		url.protocol = filename.get_parameter('proto', url.protocol);
+
+		// Fetch the db.  Follow redirects, and determine if range queries are supported.
+		const resp = await fetch(url, {headers: {'Range': 'bytes=0-99'}});
+		check_err(resp);
+
+		// Check if the server supported our range request (returned partial content)
+		if (resp.status == 206) {
+			return new HttpFile(resp, flags);
 		} else {
-			url = new URL(filename, self?.document?.baseURI ?? location);
+			return new BlobFile(await resp.blob(), flags);
 		}
-
-		// Check if the file exists + get final URL + etag + size
-		const resp = await fetch(url, {
-			method: 'head',
-		});
-		if (!resp.ok) throw new Error(`HTTP VFS:(${resp.status}) ${resp.statusText}`);
-
-		let size = resp.headers.get('content-length');
-		if (!size) throw new Error('HTTP VFS: We require that the server return a content-length header in a HEAD response.');
-		size = BigInt(size);
-		
-		const headers = {};
-		const etag = resp.headers.get('etag');
-		if (etag) {
-			headers['If-Match'] = etag;
-		}
-		const last_modified = resp.headers.get('last-modified');
-		if (last_modified) {
-			headers['If-Unmodified-Since'] = last_modified;
-		}
-
-		return new HttpFile(resp.url, size, headers, flags | SQLITE_OPEN_READONLY);
 	}
-	async delete(filename, sync) { throw new Error('Unimplementable'); }
-	async access(filename, flags) { return false; }
-	full_pathname(pathname) { return pathname; }
+	delete(_filename, _sync) { throw new Error('Unimplementable'); }
+	access(_filename, _flags) { return false; }
+	full_pathname(pathname) {
+		const url = new URL(pathname, location);
+		return String(url);
+	}
 }
 export default new Http();
