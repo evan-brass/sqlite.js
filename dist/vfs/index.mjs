c6b2f59f589816124d155aa09177368325593526
Evan Brass <brassevan@gmail.com>
2023-10-10 09:50:55 -0700

    Cleanup

--- dist/vfs/index.mjs
+++
@@ -1,6 +0,0 @@
-import './basics.mjs';
-export * from './custom.mjs';
-
-export { default as opfs} from './opfs.mjs';
-export { default as picker} from './picker.mjs';
-export { default as http} from './http.mjs';
