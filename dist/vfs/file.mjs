c314183ff4ea93679f8cb7eebb8c48d5ab03f054
Evan Brass <brassevan@gmail.com>
2023-10-13 11:02:09 -0700

    File rewrite
    Disable batch atomic so that we can test the baseline implementation.
    This is important because incremental Blob IO doesn't use batched atomic writes.

--- dist/vfs/file.mjs
+++ dist/vfs/file.mjs
@@ -1,12 +1,13 @@
 import {
-	SQLITE_OK,
+	SQLITE_OK, SQLITE_NOTFOUND,
+	SQLITE_OPEN_DELETEONCLOSE,
+	// SQLITE_IOCAP_ATOMIC, SQLITE_IOCAP_BATCH_ATOMIC,
 	SQLITE_FCNTL_BEGIN_ATOMIC_WRITE, SQLITE_FCNTL_COMMIT_ATOMIC_WRITE, SQLITE_FCNTL_ROLLBACK_ATOMIC_WRITE,
-	SQLITE_IOCAP_ATOMIC, SQLITE_IOCAP_BATCH_ATOMIC,
-	SQLITE_OPEN_DELETEONCLOSE, SQLITE_NOTFOUND
+	SQLITE_IOCAP_POWERSAFE_OVERWRITE, SQLITE_IOCAP_SAFE_APPEND,
 } from '../sqlite_def.mjs';
 
 
-function get_lock(name, options) {
+function get_lock(name, options = {mode: 'shared'}) {
 	return new Promise(res => {
 		navigator.locks.request(name, options, async lock => {
 			res(lock);
@@ -16,21 +17,30 @@ function get_lock(name, options) {
 		});
 	});
 }
+function res_name(lock_name) {
+	return lock_name + '-r';
+}
 
 export class File {
-	flags;
 	#handle;
+	
+	flags;
+	sector_size = 0;
+	
+	#lock_name;
 	#lock;
 	#res_lock;
-	#writable_stream;
-	sector_size = 0;
-	#lock_name;
-	#lock_name_res;
+
+	#blob;
+	#size;
+	#atomic = false;
+	#writable;
+	#dirty = new Set();
+
 	constructor(handle, flags, lock_name = handle.name) {
 		this.#handle = handle;
 		this.flags = flags;
 		this.#lock_name = lock_name;
-		this.#lock_name_res = this.#lock_name + '-r';
 	}
 	get handle() {
 		return this.#handle;
@@ -38,24 +48,27 @@ export class File {
 
 	// Other:
 	device_characteristics() {
-		// TODO: If createSyncAccessHandle, then no batch atomics.
-		return SQLITE_IOCAP_ATOMIC | SQLITE_IOCAP_BATCH_ATOMIC;
+		return SQLITE_IOCAP_POWERSAFE_OVERWRITE | SQLITE_IOCAP_SAFE_APPEND;
+		// TODO: Batched Atomic
+		// return SQLITE_IOCAP_ATOMIC | SQLITE_IOCAP_BATCH_ATOMIC;
 	}
 	// Locking:
 	async lock(level) {
 		if (level >= 1 && !this.#lock) {
 			// Aquire a shared lock on lock_name
-			this.#lock = await get_lock(this.#lock_name, {mode: 'shared', ifAvailable: true});
-			if (!this.#lock) return false;
+			this.#lock = await get_lock(this.#lock_name);
+
+			this.#blob = await this.#handle.getFile();
+			this.#size = this.#blob.size;
 		}
 		if (level >= 2 && !this.#res_lock) {
 			// Aquire an exclusive lock on lock_name_res or return BUSY if it isn't available.
-			this.#res_lock = await get_lock(this.#lock_name_res, {mode: 'exclusive', ifAvailable: true});
+			this.#res_lock = await get_lock(res_name(this.#lock_name), {mode: 'exclusive', ifAvailable: true});
 			if (!this.#res_lock) return false;
 		}
-		if (level >= 3 && !this.#lock.mode == 'shared') {
+		if (level >= 3 && this.#lock.mode == 'shared') {
 			// Exchange our shared lock on lock_name for an exclusive lock on lock_name
-			const new_lock_prom = get_lock(this.#lock_name);
+			const new_lock_prom = get_lock(this.#lock_name, {mode: 'exclusive'});
 			this.#lock.release();
 			this.#lock = await new_lock_prom;
 		}
@@ -65,8 +78,9 @@ export class File {
 	}
 	async unlock(level) {
 		if (level <= 1 && this.#lock?.mode == 'exclusive') {
+			const prom = get_lock(this.#lock_name);
 			this.#lock.release();
-			this.#lock = await get_lock(this.#lock_name, {mode: 'shared'});
+			this.#lock = await prom;
 			this.#res_lock.release();
 			this.#res_lock = null;
 		}
@@ -76,11 +90,8 @@ export class File {
 		}
 	}
 	async check_reserved_lock() {
-		const res = await get_lock(this.#lock_name_res, {mode: 'shared', ifAvailable: true});
-		if (res) {
-			res.release();
-		}
-		return !res;
+		const {held} = await navigator.locks.query(res_name(this.#lock_name));
+		return Boolean(held.length);
 	}
 	// IO:
 	async close() {
@@ -89,58 +100,85 @@ export class File {
 		}
 	}
 	sync() {
-		if (this.#writable_stream) throw new Error('wat?');
+		// Don't sync when in the middle of atomic write:
+		if (this.#atomic) return;
+
+		if (this.#writable) return (async () => {
+			// Close the writable stream:
+			await this.#writable.close();
+			this.#writable = null;
+			// Clear the dirty set:
+			this.#dirty.clear();
+			// Clear the File blob handle:
+			this.#blob = null;
+		})();
 	}
 	async read(offset, len) {
-		if (this.#handle.requestPermission) {
-			const res = await this.#handle.requestPermission({mode: 'read'});
-			if (res != 'granted') throw new Error("Permission denied");
-		}
 		offset = Number(offset);
-		if (this.#writable_stream) throw new Error('wat?');
-		const file = await this.#handle.getFile();
-		const section = file.slice(offset, offset + len);
-		const data = new Uint8Array(await section.arrayBuffer());
-		return data;
+		console.assert(this.#atomic === false, "We can't read in the middle of an atomic write.");
+
+		// If the offset is dirty, then we need to sync the file before reading:
+		if (this.#dirty.has(offset)) await this.sync();
+		
+		this.#blob ??= await this.#handle.getFile();
+
+		const section = this.#blob.slice(offset, offset + len);
+		return new Uint8Array(await section.arrayBuffer());
 	}
 	async write(buffer, offset) {
-		if (this.#handle.requestPermission) {
-			const res = await this.#handle.requestPermission({mode: 'readwrite'});
-			if (res != 'granted') throw new Error("Permission denied");
-		}
-		const position = Number(offset);
-		const stream = this.#writable_stream ?? await this.#handle.createWritable({keepExistingData: true});
+		offset = Number(offset);
+		this.#writable ??= await this.#handle.createWritable({keepExistingData: true});
 
-		await stream.write({type: 'write', data: buffer, position});
+		await this.#writable.seek(Number(offset));
+		await this.#writable.write(buffer);
+		// TODO: If there's more than 100 dirty pages then just mark the whole file as dirty?
+		this.#dirty.add(offset);
 
-		if (this.#writable_stream != stream) await stream.close();
+		// Update the file size:
+		this.#size = Math.max(this.#size, offset + buffer.byteLength);
 	}
-	async file_control(op, _arg) {
+	file_control(op, _arg) {
 		if (op == SQLITE_FCNTL_BEGIN_ATOMIC_WRITE) {
-			this.#writable_stream = await this.#handle.createWritable({keepExistingData: true});
-			return SQLITE_OK;
-		}
-		else if (op == SQLITE_FCNTL_COMMIT_ATOMIC_WRITE) {
-			await this.#writable_stream.close();
-			this.#writable_stream = null;
+			this.#atomic = true;
 			return SQLITE_OK;
 		}
 		else if (op == SQLITE_FCNTL_ROLLBACK_ATOMIC_WRITE) {
-			await this.#writable_stream.abort();
-			this.#writable_stream = null;
-			return SQLITE_OK;
-		} else {
+			return (async () => {
+				await this.#writable.abort();
+				this.#writable = null;
+				this.#dirty.clear();
+				// Update the file size:
+				this.#blob = await this.#handle.getFile();
+				this.#size = this.#blob.size;
+				return SQLITE_OK;
+			})();
+		}
+		else if (op == SQLITE_FCNTL_COMMIT_ATOMIC_WRITE) {
+			return (async () => {
+				await this.#writable.close();
+				this.#writable = null;
+				this.#dirty.clear();
+				return SQLITE_OK;
+			})();
+		}
+		else {
 			return SQLITE_NOTFOUND;
 		}
 	}
 	async trunc(length) {
-		if (this.#writable_stream) throw new Error('wat?');
-		const writable = await this.#handle.createWritable({keepExistingData: true});
-		await writable.truncate(Number(length));
-		await writable.close();
+		length = Number(length);
+		this.#writable ??= await this.#handle.createWritable({keepExistingData: true});
+		await this.#writable.truncate(length);
+
+		// Remove any dirty pages that are past the new length:
+		for (const offset of this.#dirty) {
+			if (offset > length) this.#dirty.delete(offset);
+		}
+
+		// Update the file size:
+		this.#size = length;
 	}
-	async size() {
-		const file = await this.#handle.getFile();
-		return BigInt(file.size);
+	size() {
+		return BigInt(this.#size);
 	}
 }
