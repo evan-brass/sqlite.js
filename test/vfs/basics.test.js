af75500e21a71b83ba6ca98aec5a17705ae27899
Evan Brass <brassevan@gmail.com>
2024-03-13 14:51:42 -0700

    testing
    basic vfs environment

---
+++ test/vfs/basics.test.js
@@ -0,0 +1,26 @@
+import { assertEquals, assertNotEquals } from 'std/assert/mod.ts';
+import { rows, Conn } from "sqloaf/conn.js";
+import 'sqloaf/vfs/basics.js';
+
+const conn = new Conn();
+await conn.open();
+
+/**
+ * vfs/basics provides environment access to random numbers (through crypto.getRandomValues) and the current time (through Date.now).
+ * I'm not currently checking that these are accurate, just that they don't throw.
+ */
+
+Deno.test(async function randomness() {	
+	const [{random: t1}] = await rows(conn.sql`SELECT hex(randomblob(20)) AS random;`);
+	const [{random: t2}] = await rows(conn.sql`SELECT hex(randomblob(20)) AS random;`);
+
+	assertEquals(typeof t1, 'string');
+	assertEquals(t1.length, 40);
+	assertNotEquals(t1, t2);
+});
+
+Deno.test(async function datetime() {
+	const [{now}] = await rows(conn.sql`SELECT datetime() AS now;`);
+
+	assertEquals(typeof now, 'string');
+});
